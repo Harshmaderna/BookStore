63dfefcbd2de4b6665d08bcf964fff7e0f290c42
harsh maderna <madernaharsh84@gmail.com>
2025-08-18 17:23:01 +0530

    clear

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,6 @@ const Login = () => {
         <dialog id="my_modal_3" className="modal">
           <div className="modal-box dark:bg-slate-900 dark:text-white">
             <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
               <button className="btn dark:bg-slate-900 dark:text-white btn-sm btn-circle btn-ghost absolute right-2 top-2">
                 ✕
               </button>
